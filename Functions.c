e7eeab774fd2b66eb4750c801302adb7441d3fb1
ChristinMelad <christinmelad@yahoo.com>
2023-08-20 12:25:53 +0300

    updates

--- Functions.c
+++ Functions.c
@@ -1,18 +1,18 @@
 #include <unistd.h>
 /**
- * _putchar - writes the char to stdout
- * @c: character
- * Return: on success 1
- */
+*_putchar - writes the char to stdout
+*@c: character
+*Return: on success 1
+*/
 int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
 
 /**
- * _puts - prints string
- * @str: the string
- * Return: void
+*_puts - prints string
+*@str: the string
+*Return: void
 */
 void _puts(char *str)
 {
@@ -25,18 +25,19 @@ _putchar('\n');
 }
 
 /**
- * len - returns the length of string
- * Return: string length
+*len - returns the length of string
+*Return: string length
+*@s:string to be printed
 */
 int len(char *s)
 {
 int i = 0;
 if (s != NULL)
 {
-    while (s[i] != '\0')
-    {
-        i++;
-    }
+while (s[i] != '\0')
+{
+i++;
+}
 }
 return (i);
 }
