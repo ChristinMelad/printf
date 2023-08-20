4fc1b96bb80f0ea3be087631c56e70702c138b2d
ChristinMelad <christinmelad@yahoo.com>
2023-08-20 13:09:39 +0300

    main header

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+/*My Functions*/
+int _putchar(char c);
+void _puts(char *str);
+int len(char *s);
+int _printf(const char *format, ...);
+
+
+#endif
