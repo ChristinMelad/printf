5990a1d468db9c5a8df7d0314cfd9f78c644a8da
ChristinMelad <christinmelad@yahoo.com>
2023-08-22 15:26:25 +0300

    updates

--- _printf.c
+++ _printf.c
@@ -1,32 +1,47 @@
 #include "main.h"
+
 /**
- *_printf - function that prints the characters
- *@format: string
- *Return: format
- */
+*_printf - function that prints the characters
+*@format: our main string
+*Return: numbers of the characters
+*/
 
 int _printf(const char *format, ...)
 {
-	int numbers = 0;
+	specifiers arr[] = {
+		{"%c", printc},
+		{"%s", print_string},
+		{"%%", print_percentage},
+		{"%d", print_decimal},
+		{"%i", print_integer},
+	};
 
-	va_list args;
+va_list args;
+int i = 0, j, numbers = 0;
 
-	va_start(args, format);
+va_start(args, format);
 
-	while (*format != '\0')
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+return (-1);
+
+First_loop:
+
+while (format[i] != '\0')
+{
+	j = 4;
+	while (j >= 0)
 	{
-		if (*format == '%')
+		if (arr[j].c[0] == format[i] && arr[j].c[1] == format[i + 1])
 		{
-			format++;
-			numbers = selector(format, args, numbers);
-			format++;
+			numbers += arr[j].func(args);
+			i += 2;
+			goto First_loop;
 		}
-		else
-		{
-			_putchar(*format);
-			numbers++;
-			format++;
-		}
+		j--;
+	}
+	_putchar(format[i]);
+	numbers++;
+	i++;
 	}
 	va_end(args);
 	return (numbers);
--- main.h
+++ main.h
@@ -8,9 +8,27 @@
 #include <string.h>
 
 int _putchar(char c);
-int print_string(va_list args, int numbers);
-int printc(va_list args, int numbers);
-int selector(const char *format, va_list args, int numbers);
 int _printf(const char *format, ...);
-int print_integer(va_list args, int numbers);
-#endif
+int printc(va_list args);
+int print_string(va_list args);
+int print_percentage(void);
+int print_decimal(va_list args);
+int print_integer(va_list args);
+int str_len(char *s);
+int str_lenc(const char *s);
+
+
+/**
+*struct my_specifiers - match the conversion specifiers
+*@c: type pointer of the specifier
+*@func: pointer to function
+*/
+typedef struct my_specifiers
+{
+	char *c;
+	int (*func)();
+
+} specifiers;
+
+
+#endif /*MAIN_H*/
